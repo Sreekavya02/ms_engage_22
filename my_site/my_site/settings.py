f69fceaa266b0d1ce5fbc090cd51b521529d8106
Sreekavya02 <81862118+Sreekavya02@users.noreply.github.com>
2022-05-28 21:28:14 +0530

    Update settings.py

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)(irl=8^nztk1-pek89=h7r^cov4nsad==*3o8yp-lt78#3!#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','engage22recommendation.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
